d61d465797e9aee403e2102ef4e4e3995eb50a0b
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2024-05-04 19:15:23 +0530

    Added upto user authentication controller

--- api/app.js
+++ api/app.js
@@ -1,9 +1,13 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/auth.route.js"
+import postRoute from "./routes/post.route.js";
 const app = express();
+app.use(express.json());
+app.use(cookieParser())
 
-app.use("/api/test", (req, res)=>{
-    res.send("hi there")
-})
+app.use("/api/auth/", authRoute);
+app.use("/api/test/", postRoute);
 
 app.listen(8800,()=>{
     console.log("Server is running!");
