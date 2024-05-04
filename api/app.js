1e04df3c833097e6e3bbbb02ecab3fb6e795726a
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2024-05-04 11:26:32 +0530

    Added backned

---
+++ api/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.use("/api/test", (req, res)=>{
+    res.send("hi there")
+})
+
+app.listen(8800,()=>{
+    console.log("Server is running!");
+} )
