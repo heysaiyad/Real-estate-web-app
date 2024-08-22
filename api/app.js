375120bbdb102632379a027bd8014b9afc0fd854
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2024-08-23 00:04:25 +0530

    fix:fixed the database issue

--- api/app.js
+++ api/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
+import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js"
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
-
+dotenv.config();
 
 const app = express();
 // app.use(cors({origin:process.env.CLINET_URL, credentials:true}))
@@ -18,6 +19,11 @@ app.use("/api/users", userRoute);
 app.use("/api/auth/", authRoute);
 app.use("/api/test/", testRoute);
 app.use("/api/posts/", postRoute);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
 
 app.listen(8800,()=>{
     console.log("Server is running!");
