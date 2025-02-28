3220ae0fad277a4a53073c41614c5dc0d7c8b792
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2025-02-28 16:57:12 +0530

    Add route for saving a post

--- client/src/routes/singlePage/singlePage.jsx
+++ client/src/routes/singlePage/singlePage.jsx
@@ -2,11 +2,32 @@ import "./singlePage.scss";
 import Slider from "../../components/slider/Slider";
 import Map from "../../components/map/Map";
 import { singlePostData, userData } from "../../lib/dummydata";
-import { useLoaderData } from "react-router-dom";
+import { useNavigate, useLoaderData } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import apiRequest from "../../lib/apiRequest";
+import { useState } from "react";
 
 function SinglePage() {
   const post = useLoaderData();
-  // console.log(post);
+  const [saved, setSaved] = useState(post.isSaved);
+  const {currentUser} = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleSave = async () => {
+    if (!currentUser) {
+      navigate("/login");
+    }
+    // AFTER REACT 19 UPDATE TO USEOPTIMISTIK HOOK
+    setSaved((prev) => !prev);
+    try {
+      await apiRequest.post("/users/save", { postId: post.id });
+    } catch (err) {
+      console.log(err);
+      setSaved((prev) => !prev);
+    }
+  };
   
   return (
     <div className="singlePage">
@@ -120,9 +141,11 @@ function SinglePage() {
               <img src="/chat.png" alt="" />
               Send a Message
             </button>
-            <button>
+            <button onClick={ handleSave} 
+            style={{backgroundColor: saved ? "#fece51" : "white"}}>
+            
               <img src="/save.png" alt="" />
-              Save the Place
+              {saved ? "Place Saved" : "Save the Place"}
             </button>
           </div>
         </div>
--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,4 +1,5 @@
 import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
 
 export const getPosts  = async(req, res)=>{
     const query = req.query;
@@ -26,33 +27,49 @@ export const getPosts  = async(req, res)=>{
         res.status(500).json({message:"Failed to get posts"})
     }
 }
+  
+export const getPost = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const post = await prisma.post.findUnique({
+      where: { id },
+      include: {
+        postDetail: true,
+        user: {
+          select: {
+            username: true,
+            avatar: true,
+          },
+        },
+      },
+    });
 
-export const getPost  = async(req, res)=>{
+    const token = req.cookies?.token;
 
-    const id = req.params.id;
-    try{
-        const post = await prisma.post.findUnique({
-            where: { id },
-            include: {
-              postDetail: true,
-              user: {
-                select: {
-                  username: true,
-                  avatar: true,
-                },
+    if (token) {
+      jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+        if (!err) {
+          const saved = await prisma.savedPost.findUnique({
+            where: {
+              userId_postId: {
+                postId: id,
+                userId: payload.id,
               },
             },
           });
-        if (!post) {
-            return res.status(404).json({ message: "Post not found" });
+          return res.status(200).json({ ...post, isSaved: saved ? true : false });
+        } else {
+          return res.status(200).json({ ...post, isSaved: false });
         }
-        res.status(200).json(post);
-
-    }catch(err){
-        console.log(err);
-        res.status(500).json({message:"Failed to get post"})
+      });
+    } else {
+      return res.status(200).json({ ...post, isSaved: false });
     }
-}
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get post" });
+  }
+};
 
 export const addPost = async(req, res)=>{
     const body = req.body;
